9704f6df77b5c3ed2f6b6558d28a9560ab51fd34
Gabriel Mota da Câmara <gabriel.mota.cam@gmail.com>
2023-09-21 00:06:26 -0300

    ...

--- backend/seederScript.js
+++ backend/seederScript.js
@@ -1,24 +1,24 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const productsData = require('./data/products');
-const connectDB = require('./config/db');
-const Product = require('./models/Product');
+const productData = require("./data/products");
+const connectDB = require("./config/db");
+const Product = require("./models/Product");
 
 connectDB();
 
 const importData = async () => {
-    try {
-        await Product.deleteMany({});
+  try {
+    await Product.deleteMany({});
 
-        await Product.insertMany(productsData);
+    await Product.insertMany(productData);
 
-        console.log("Dados importados com sucesso!");
+    console.log("Data Import Success");
 
-        process.exit();
-    } catch (error) {
-        console.error("Erro na importação de dados!");
-        process.exit(1);
-    }
+    process.exit();
+  } catch (error) {
+    console.error("Error with data import", error);
+    process.exit(1);
+  }
 };
 
 importData();
